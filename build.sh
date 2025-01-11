4b3b9fd19d657177171fac3dc287a0d02fbb93ab
Igor V. Gulyaev <igulyaev@gmail.com>
2025-01-11 04:04:04 +0300

    * in progress

--- build.sh
+++ build.sh
@@ -7,9 +7,10 @@ set +a
 
 #env
 #exit
+cp -a $(pwd)/src-${SRC_VERSION}/sys/* ${BASEDIR}/sys/
 cd ${BASEDIR}
 #make cleanworld
-make buildworld
+#make buildworld
 make buildkernel
 cp ${MAKEOBJDIRPREFIX}/${TARGET_ARCH}.arm/${SRCDIR}/sys/${KERNCONF}/kernel.bin  /tank/Share/tftp/stora-${SRC_VERSION}.bin
 #make installworld
